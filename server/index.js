9702169338c8eb734739936f116724728493e778
James Encinas <ljencinas87@gmail.com>
2026-05-15 17:41:59 -0300

    fix: agregado localhost:3000 a origins permitidos de CORS

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ const app = express();
 const PORT = 3000;
 
 app.use(cors({
-    origin: 'http://127.0.0.1:5500',
+    origin: ['http://127.0.0.1:5500', 'http://localhost:3000'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type']
 }));
